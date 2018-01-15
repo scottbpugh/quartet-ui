5f3561641afc3dfacee2b0705499567811e73eb9
Loic Duros <loic.duros@gmail.com>
2018-01-15 14:37:38 -0500

    removed variable

--- src/App.test.js
+++ src/App.test.js
@@ -15,7 +15,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-tt;
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
