da1176b9d940ce825bc90fe6cdc86bd2d49dac96
mmicherda <mmicherda@andea.com>
2022-03-02 21:56:43 +0100

    Change script for DST pop-up

--- src/components/elements/Clock.js
+++ src/components/elements/Clock.js
@@ -13,42 +13,42 @@ constructor(props) {
         timeTochangeInUS: null,
         timeTochangeInUE: null,
         visibility: true,
-        tempo: 0
+        tempo: 0,
+        isChecked: sessionStorage.getItem("DSTCheckbox") || false
     };
+    this.myInterval = null;
 }
 componentDidMount() {
-        const myInterval = setInterval(() => {
-            if (this.state.tempo >= 30) {
-                this.setState({
-                    visibility: false
-                })
-                clearInterval(myInterval);
-              } else {
-                const date = new Date();
-                this.setState({ 
-                    tempo: this.state.tempo+1,
-                    newTime: date.toLocaleTimeString(), 
-                    day: date.getDate(),
-                    month: date.getMonth() + 1,
-                    year: date.getFullYear(),
-                });
-            // How to pass arguments to function:
-            // this.displayTimeChange(timezoneArray, countryState);
-            this.displayTimeChange(timeUS, "timeTochangeInUS");
-            this.displayTimeChange(timeEU, "timeTochangeInUE");
-            // const offset = new Date().getTimezoneOffset();
-            // console.log(offset);
-              }
-            
-        }, 1000);
-        
+        if(this.state.isChecked === false) {
+            this.myInterval = setInterval(() => {
+                if (this.state.tempo >= 30) {
+                    this.setState({
+                        visibility: false
+                    })
+                    clearInterval(this.myInterval);
+                  } else {
+                    const date = new Date();
+                    this.setState({ 
+                        tempo: this.state.tempo+1,
+                        newTime: date.toLocaleTimeString(), 
+                        day: date.getDate(),
+                        month: date.getMonth() + 1,
+                        year: date.getFullYear(),
+                    });
+                // How to pass arguments to function:
+                // this.displayTimeChange(timezoneArray, countryState);
+                this.displayTimeChange(timeUS, "timeTochangeInUS");
+                this.displayTimeChange(timeEU, "timeTochangeInUE");
+                };
+            }, 1000);
+        };
 }
 hidePopup = (e) => {
     e.preventDefault();
-    console.log("Clicked")
     this.setState({
         visibility: false,
     })
+    clearInterval(this.myInterval);
 };
 msToTime = (duration) => {
     var milliseconds = (duration),
@@ -84,7 +84,17 @@ displayTimeChange = (timezoneArray, countryState) => {
         }
     }
 }
-
+handleCheckbox = (e) => {
+    e.preventDefault();
+    this.setState({ isChecked: e.target.checked });
+    sessionStorage.setItem("DSTCheckbox", this.state.isChecked);
+}
+remindLater = (e) => {
+    e.preventDefault();
+    this.setState({ isChecked: !this.state.isChecked });
+    sessionStorage.setItem("DSTCheckbox", this.state.isChecked);
+    this.hidePopup(e);
+}
 render(){
     return(
         this.state.timeTochangeInUE && this.state.timeTochangeInUS || 
@@ -102,10 +112,30 @@ render(){
         >
             <div onClick={this.hidePopup} className="pt-button pt-icon-cross close-button-popup"></div>
             {/* <div>Today is: {this.state.month}/{this.state.day}/{this.state.year} {this.state.newTime}</div> */}
-            {/* <div></div> */}
-            <div className="popup-header">Next Daylight Saving Time (DST):</div>
+            <div className="popup-header">Next Daylight Saving Time Shift:</div>
             {this.state.timeTochangeInUE ? <div className="popup-text"><span className="pt-icon-flag"></span> Europe/EU: {this.state.timeTochangeInUE}</div> : null}
             {this.state.timeTochangeInUS ? <div className="popup-text"><span className="pt-icon-flag"></span> USA: {this.state.timeTochangeInUS}</div> : null}
+            {
+            <div>
+                {/* <input 
+
+                    name="Active"
+                    className="pt-control pt-switch"
+                    type="checkbox" 
+                    checked={this.state.isChecked} 
+                    onChange={this.handleCheckbox}
+                />
+                Remind on application restart */}
+                <div 
+                className="pt-button pt-icon-remove"
+                    // type="checkbox" 
+                    // checked={this.state.isChecked} 
+                    onClick={this.remindLater}
+                >
+                   Close and remind on application restart
+                </div>
+            </div>
+            }
         </div>
         :
         null
