200f88e2539eee653608dfe1d3b3adddf66ffc3f
mmicherda <mmicherda@andea.com>
2022-05-05 18:09:26 +0200

    Delete pop-up for DST in USA

--- src/components/elements/Clock.js
+++ src/components/elements/Clock.js
@@ -70,7 +70,7 @@ displayTimeChange = (timezoneArray, countryState) => {
         // const language = window.navigator.userLanguage || window.navigator.language;
         // console.log(language);
         if (newDate - nowDate > 0 ) {
-            if (newDate - nowDate > 19209600000) {
+            if (newDate - nowDate > 1209600000) {
                 break;
             } else {
                 const value = this.msToTime(newDate - nowDate);
